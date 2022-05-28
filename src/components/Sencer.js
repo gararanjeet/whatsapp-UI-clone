0f4c29bca82c823aa858f42f430718cb5e343208
gararanjeet <ranjeetgara123@gmail.com>
2022-05-28 22:52:31 +0530

    Updates

--- src/components/Sencer.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./css/sencer.css";
-
-export default function Sencer() {
-  return <div className="sencer"></div>;
-}
--- src/components/Speaker.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import "./css/speaker.css";
-
-export default function Speaker() {
-  return <div className="speaker"></div>;
-}
